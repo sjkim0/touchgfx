e06c87f55ef96e6c7b993d4b978dcaefddd6a50e
ksj10111011@naver.com <ksj10111011@naver.com>
2024-09-30 11:54:47 +0900

    check : 0x90000000 area setting

--- _2_load_firmware_new/src/bsp/bsp.c
+++ _2_load_firmware_new/src/bsp/bsp.c
@@ -137,10 +137,10 @@ void MPU_Config(void)
   MPU_InitStruct.Size = MPU_REGION_SIZE_16MB;
   MPU_InitStruct.SubRegionDisable = 0x0;
   MPU_InitStruct.DisableExec = MPU_INSTRUCTION_ACCESS_ENABLE;
-  MPU_InitStruct.IsShareable = MPU_ACCESS_NOT_SHAREABLE;
-  MPU_InitStruct.IsCacheable = MPU_ACCESS_NOT_CACHEABLE;
+  MPU_InitStruct.IsShareable = MPU_ACCESS_SHAREABLE;
+  MPU_InitStruct.IsCacheable = MPU_ACCESS_CACHEABLE;
 
-  HAL_MPU_ConfigRegion(&MPU_InitStruct);
+//  HAL_MPU_ConfigRegion(&MPU_InitStruct);
 
   /** Initializes and configures the Region and the memory to be protected
   */
